b2635b66235b636ade4952315abf5cf2bd5559c0
kbberker <kbbuk90@gmail.com>
2019-04-24 15:11:52 +0100

    Send login req to server-return JSON user or error

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import API from "../API";
+import { Link } from "react-router-dom";
 
 class Login extends Component {
   state = {
@@ -11,23 +12,29 @@ class Login extends Component {
     this.setState({ [event.target.name]: event.target.value });
   };
 
+  handleSubmit = () => {
+    API.siginin(this.state).then(console.log);
+  };
+
   render() {
     return (
       <div className="login">
-        <label for="email">Email Address</label>
+        <label htmlFor="email">Email Address</label>
         <input
           type="email"
           name="email"
           id="email"
           onChange={this.updateUserInput}
         />
-        <label for="password">Password</label>
+        <label htmlFor="password">Password</label>
         <input
           type="password"
           name="password"
           id="password"
           onChange={this.updateUserInput}
         />
+        <button onClick={this.handleSubmit}>Login</button>
+        <Link to="/createaccount">Create Account</Link>
       </div>
     );
   }
